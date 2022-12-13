86cb80bc17c4d1ef9ea037ca4cf68fbdae1fceea
abjunior92 <junior.berselli@gmail.com>
2022-12-13 15:35:45 +0100

    changes

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -34,6 +34,8 @@ const Home = () => {
       <Head>
         <title>Bersy Home Page</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#ef4444"></meta>
       </Head>
 
